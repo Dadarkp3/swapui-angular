0b5af95c242b5f2ca0c47c70787b88f8acfe9552
Daiane Silva <adm.daianesilva@gmail.com>
2019-11-10 18:15:26 -0300

    rest of the unit test of the components

--- src/app/components/people-grid/people-grid.component.spec.ts
+++ src/app/components/people-grid/people-grid.component.spec.ts
@@ -1,25 +1,71 @@
-// import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-// import { PeopleGridComponent } from "./people-grid.component";
+import { PeopleGridComponent } from "./people-grid.component";
+import { LoaderComponent } from "../loader/loader.component";
+import { PeopleCardComponent } from "../people-card/people-card.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { People } from "src/app/models/People";
+import { By } from "@angular/platform-browser";
 
-// describe("PeopleGridComponent", () => {
-//   let component: PeopleGridComponent;
-//   let fixture: ComponentFixture<PeopleGridComponent>;
+const peoples: People[] = [];
+const people: People = new People();
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [PeopleGridComponent]
-//     }).compileComponents();
-//   }));
+describe("PeopleGridComponent", () => {
+  let component: PeopleGridComponent;
+  let fixture: ComponentFixture<PeopleGridComponent>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PeopleGridComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [PeopleGridComponent, LoaderComponent, PeopleCardComponent],
+      imports: [FontAwesomeModule],
+      schemas: []
+    }).compileComponents();
+  }));
 
-//   it("should create", () => {
-//     component.isLoading = true;
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PeopleGridComponent);
+    component = fixture.componentInstance;
+    component.peoples = peoples;
+    component.isLoading = false;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should show empty people messsage", () => {
+    component.peoples = [];
+    fixture.detectChanges();
+    let error = fixture.debugElement.query(By.css("#error")).nativeElement;
+    expect(error.innerText).toBe(
+      "You just looked for someone that isn't born yet!"
+    );
+  });
+
+  it("it should retourn true for empty people", () => {
+    component.peoples = [];
+    fixture.detectChanges();
+    expect(component.isEmpty()).toBeTruthy();
+  });
+
+  it("it should retourn false for empty people", () => {
+    people.src =
+      "https://www.sideshow.com/storage/product-images/903109/luke-skywalker_star-wars_feature.jpg";
+    peoples.push(people);
+    fixture.detectChanges();
+    expect(component.isEmpty()).toBeFalsy();
+  });
+
+  it("should show cards", () => {
+    people.src =
+      "https://www.sideshow.com/storage/product-images/903109/luke-skywalker_star-wars_feature.jpg";
+    peoples.push(people);
+    fixture.detectChanges();
+
+    let component = fixture.debugElement.query(By.css("app-people-card"))
+      .nativeElement;
+    expect(component).toBeTruthy();
+  });
+});
